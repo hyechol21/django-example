d2a43f23b3bfa4cb6b0a0482d4ae00d78682870b
hyechol21 <like3orange@gmail.com>
2024-03-21 12:38:49 +0900

    add

---
+++ drf_myweb/example/views.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets, permissions, generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+
+
+# Create your views here.
