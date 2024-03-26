c5a09fb0b191a1a6f5f10abc8a252b96286279a6
hyechol21 <like3orange@gmail.com>
2024-03-26 23:26:30 +0900

    add generics

--- drf_myweb/example/urls.py
+++ drf_myweb/example/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import HelloAPI, booksAPI, bookAPI, BooksAPI, BookAPI
+from .views import BooksAPIMixins, BookAPIMixins, BooksAPIGenerics, BookAPIGenerics
 
 
 urlpatterns = [
@@ -8,4 +9,8 @@ urlpatterns = [
     path('fbv/book/<int:bid>/', bookAPI),
     path('cbv/books/', BooksAPI.as_view()),
     path('cbv/book/<int:bid>/', BookAPI.as_view()),
+    path('mixin/books/', BooksAPIMixins.as_view()),
+    path('mixin/book/<int:bid>/', BookAPIMixins.as_view()),
+    path('generic/books/', BooksAPIGenerics.as_view()),
+    path('generic/book/<int:bid>/', BookAPIGenerics.as_view()),
 ]
--- drf_myweb/example/views.py
+++ drf_myweb/example/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions, generics, status
+from rest_framework import viewsets, mixins, generics, status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
@@ -32,8 +32,6 @@ def bookAPI(request, bid):  # /book/bid
     return Response(serializer.data, status=status.HTTP_200_OK)
     
     
-
-
 # CBV
 class BooksAPI(APIView):
     def get(self, request):
@@ -54,3 +52,39 @@ class BookAPI(APIView):
         book = get_object_or_404(Book, bid=bid)
         serializer = BookSerializer(book)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    
+# mixins
+class BooksAPIMixins(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+
+class BookAPIMixins(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'bid'
+    
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+    
+
+# generics
+class BooksAPIGenerics(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+
+class BookAPIGenerics(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'bid'
