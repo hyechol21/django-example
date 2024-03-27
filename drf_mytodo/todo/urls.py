65014e92216bb99b0708071fa4f6fd6fe2001cf4
hyechol21 <like3orange@gmail.com>
2024-03-27 18:04:48 +0900

    update drf_mytodo

--- drf_mytodo/todo/urls.py
+++ drf_mytodo/todo/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import TodoAPIView
+from .views import TodosAPIView, TodoAPIView, DoneTodosAPIView, DoneTodoAPIView
 
 
 urlpatterns = [
-    path('todo', TodoAPIView.as_view())
+    path('todo/', TodosAPIView.as_view()),
+    path('todo/<int:pk>/', TodoAPIView.as_view()),
+    path('done/', DoneTodosAPIView.as_view()),
+    path('done/<int:pk>/', DoneTodoAPIView.as_view())
 ]
